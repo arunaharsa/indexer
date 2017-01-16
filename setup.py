8355c023ea047f2858b056926176830fe2293e00
Jacky Wijaya <jackydekoruma.com>
2017-01-16 12:58:53 +0700

    add base indexer

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+  name = 'indexer',
+  packages = ['indexer'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A package for indexer algolia',
+  author = 'Dekoruma',
+  author_email = 'hello@dekoruma.com',
+  url = 'https://github.com/peterldowns/mypackage', # use the URL to the github repo
+  keywords = ['dekoruma'],
+  classifiers = [],
+)
